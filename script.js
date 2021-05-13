a4f16dd79c3594bdebba1fc791e19347f611c591
Sayful Islam Shakib <connect.sayful@gmail.com>
2021-05-13 21:29:53 +0600

    Final submit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let form = document.querySelector('form');
 let todoUl = document.querySelector('#items');
 let completeUl = document.querySelector('.complete-list ul');
 
-//funtions
+
+// functions
 let createTask = function (task) {
   let listItem = document.createElement('li');
   let checkBox = document.createElement('input');
@@ -22,15 +23,15 @@ let addTask = function (event) {
   event.preventDefault();
   let listItem = createTask(newTask.value);
   todoUl.appendChild(listItem);
-  newTask.value = '';
-  //bind the new list item to the incomplete list
+  newTask.value = "";
+  // bind the new list item to the incomplete list
   bindInCompleteItems(listItem, completeTask);
 }
 
 let completeTask = function () {
   let listItem = this.parentNode;
   let deleteBtn = document.createElement('button');
-  deleteBtn.innerText = 'delete';
+  deleteBtn.innerText = 'Delete';
   deleteBtn.className = 'delete';
   listItem.appendChild(deleteBtn);
 
@@ -38,20 +39,30 @@ let completeTask = function () {
   checkBox.remove();
   completeUl.appendChild(listItem);
   bindCompleteItems(listItem, deleteTask);
-
-  let deleteTask = function () {
-    let listItem = this.parentNode;
-    let ul = listItem.parentNode;
-    ul.removeChild(listItem);
-  }
 }
 
-let bindInCompleteItems = function (taskItem, checkboxclick) {
-  let checkBox = taskItem.querySelector('input[type = "checkBox"]');
-  checkBox.onchange = checkboxclick;
+let deleteTask = function () {
+  let listItem = this.parentNode;
+  let ul = listItem.parentNode;
+  ul.removeChild(listItem);
+}
+
+let bindInCompleteItems = function (taskItem, checkboxClick) {
+  let checkBox = taskItem.querySelector('input[type="checkbox"]');
+  checkBox.onchange = checkboxClick;
 }
 
 let bindCompleteItems = function (taskItem, deleteButtonClick) {
   let deleteButton = taskItem.querySelector('.delete');
-  deleteButton.onClick = deleteButtonClick;
+  deleteButton.onclick = deleteButtonClick;
 }
+
+// for (let i = 0; i < todoUl.children.length; i++) {
+//   bindInCompleteItems(todoUl.children[i], completeTask);
+// }
+
+// for (let i = 0; i < completeUl.children.length; i++) {
+//   bindCompleteItems(completeUl.children[i], deleteTask);
+// }
+
+form.addEventListener('submit', addTask);
